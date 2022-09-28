7cd7ddc54c757f1f7561a1ae47c18c1108d1ad9e
Miguel <miguelmartinezmiquel@gmail.com>
2022-09-28 15:45:10 +0200

    Added flux in the amorphous cross section and possibility of no neighbours in the hamilatonian.

--- functions.py
+++ functions.py
@@ -124,46 +124,70 @@ def H_hopp(r, phi, t1, t2, lamb):
 
     return RadialHopp(r) * 0.5 * H_off
 
-def H_offdiag(n_sites, n_orb, n_neighbours, L_x, L_y, x, y, t1, t2, lamb, boundary):
+def Peierls(x0, x1, y0, y1, B):
+    # Calculates the Peierls phase betwee sites 0 and 1.
+    # x0, x1, y0, y1: Coordinates of the sites
+    # Magnetic field through the cross-section
+    if x1 != x0:
+        phase = B * ((y1 - y0) / (x1 - x0)) * 0.5 * (x1 **2 + x1 * x0) + B * y0 * (x1 - x0)
+    else:
+        phase = 0
+    return np.exp(1j * 2 * pi * phase)
+
+def H_offdiag(n_sites, n_orb, L_x, L_y, x, y, t1, t2, lamb, B, boundary, n_neighbours=None):
     # Generates the Hamiltonian for a 3D insulator with the parameters specified
     # t2 = 0  we are in the DIII class (S = - sigma0 x sigma_y), if t2 is non-zero we are in the AII class
     # n_sites: Number of sites in the lattice
     # n_orb: Number of orbitals in the model
-    # n_neighbours: Number of fixed neighbours
     # x: x position of the sites in the RPS
     # y: y position of the sites in the RPS
     # M, t1, t2, lamb: Parameters of the AII model
+    # B: Magnetic field through the cross-section of the wire
     # Boundary: String with values "Open" or "Closed" which selects the boundary we want
+    # n_neighbours: Number of fixed neighbours (optional)
 
-    # Declarations
-    H = np.zeros((n_sites * n_orb, n_sites * n_orb), complex)              # Declaration of the matrix hamiltonian
-    matrix_neighbours = np.tile(np.arange(n_sites), (n_sites, 1))          # Declaration matrix of neighbours
-    matrix_dist = np.zeros((n_sites, n_sites))                             # Declaration matrix of dists
-    matrix_phis = np.zeros((n_sites, n_sites))                             # Declaration matrix of phis
+    H = np.zeros((n_sites * n_orb, n_sites * n_orb), complex)  # Declaration of the matrix hamiltonian
 
-    # Off-diagonal hopping terms and neighbour structure
-    cont = 1  # Starts at 1 to avoid calculating diagonal properties which we do not need
-    for index1 in range(0, n_sites):
-        for index2 in range(cont, n_sites):
-            r, phi = displacement2D(x[index1], y[index1], x[index2], y[index2], L_x, L_y, boundary)
-            matrix_dist[index1, index2], matrix_phis[index1, index2], = r, phi
-            matrix_dist[index2, index1], matrix_phis[index2, index1], = r, phi + pi
+    if n_neighbours is not None:
+        # Declarations
+        matrix_neighbours = np.tile(np.arange(n_sites), (n_sites, 1))          # Declaration matrix of neighbours
+        matrix_dist = np.zeros((n_sites, n_sites))                             # Declaration matrix of dists
+        matrix_phis = np.zeros((n_sites, n_sites))                             # Declaration matrix of phis
 
-        # Sorting
-        idx = matrix_dist[index1, :].argsort()                         # Sorting the distances from minimum to maximum
-        matrix_neighbours[index1, :] = matrix_neighbours[index1, idx]  # Ordered neighbours
-        matrix_dist[index1, :] = matrix_dist[index1, idx]              # Ordered distances
-        matrix_phis[index1, :] = matrix_phis[index1, idx]              # Ordered phis
+        # Loop through the different sites of the lattice
+        cont = 1  # Starts at 1 to avoid calculating diagonal properties which we do not need
+        for site1 in range(0, n_sites):
+            for site2 in range(cont, n_sites):
+                r, phi = displacement2D(x[site1], y[site1], x[site2], y[site2], L_x, L_y, boundary)
+                matrix_dist[site1, site2], matrix_phis[site1, site2], = r, phi
+                matrix_dist[site2, site1], matrix_phis[site2, site1], = r, phi + pi
 
-        # Hamiltonian for the nearest neighbour contributions
-        for index2 in range(1, n_neighbours + 1):
-            row = index1 * n_orb
-            column = int(matrix_neighbours[index1, index2]) * n_orb
-            r, phi = matrix_dist[index1, index2], matrix_phis[index1, index2]
-            H[row: row + n_orb, column: column + n_orb] = H_hopp(r, phi, t1, t2, lamb)
-            H[column: column + n_orb, row: row + n_orb] = np.conj(H[row: row + n_orb, column: column + n_orb]).T
+            # Sorting
+            idx = matrix_dist[site1, :].argsort()                        # Sorting the distances from minimum to maximum
+            matrix_neighbours[site1, :] = matrix_neighbours[site1, idx]  # Ordered neighbours
+            matrix_dist[site1, :] = matrix_dist[site1, idx]              # Ordered distances
+            matrix_phis[site1, :] = matrix_phis[site1, idx]              # Ordered phis
 
-        cont = cont + 1
+            # Hamiltonian for the nearest neighbour contributions
+            for index_neigh in range(1, n_neighbours + 1):
+                neighbour = matrix_neighbours[site1, index_neigh]
+                row, column = site1 * n_orb, neighbour * n_orb
+                r, phi = matrix_dist[site1, index_neigh], matrix_phis[site1, index_neigh]
+                x1, x2, y1, y2 = x[site1], x[neighbour], y[site1], y[neighbour]
+                H[row: row + n_orb, column: column + n_orb] = H_hopp(r, phi, t1, t2, lamb) * Peierls(x1, x2, y1, y2, B)
+                H[column: column + n_orb, row: row + n_orb] = np.conj(H[row: row + n_orb, column: column + n_orb]).T
+            cont = cont + 1
+
+    else:
+        # Loop through the different sites of the lattice
+        cont = 1  # Starts at 1 to avoid calculating diagonal properties which we do not need
+        for site1 in range(0, n_sites):
+            for site2 in range(cont, n_sites):
+                r, phi = displacement2D(x[site1], y[site1], x[site2], y[site2], L_x, L_y, boundary)
+                row, column = site1 * n_orb, site2 * n_orb
+                H[row: row + n_orb, column: column + n_orb] = H_hopp(r, phi, t1, t2, lamb) * Peierls(x[site1], x[site2], y[site1], y[site2], B)
+                H[column: column + n_orb, row: row + n_orb] = np.conj(H[row: row + n_orb, column: column + n_orb]).T
+            cont = cont + 1
 
     return H
 
--- t_inv_NW.py
+++ t_inv_NW.py
@@ -14,14 +14,14 @@ start_time = time.time()
 # Parameters of the model
 n_orb = 4                           # Number of orbitals per site
 n_neighbours = 6                    # Number of neighbours
-width = 0                         # Width of the gaussian for the WT model
-M = 0                               # Mass parameter in units of t1
+width = 0.1                           # Width of the gaussian for the WT model
+M = 2                               # Mass parameter in units of t1
 t1, t2, lamb = 1, 0, 1              # Hopping and spin-orbit coupling in WT model
-mu = 0                              # Disorder strength
-kz = np.linspace(-pi, pi, 101)      # Momentum space
+flux = 0.5                            # Magnetic flux through the cross-section
+kz = np.linspace(-pi, pi, 1001)      # Momentum space
 
 # Lattice definition
-L_x, L_y= 8, 8                             # In units of a (average bond length)
+L_x, L_y= 9, 9                             # In units of a (average bond length)
 n_sites = int(L_x * L_y)                   # Number of sites in the lattice
 n_states = n_sites * n_orb                 # Number of basis states
 n_particles = int(n_states / 2)            # Number of filled states
@@ -29,31 +29,43 @@ sites = np.arange(0, L_x * L_y)            # Array with the number of each site
 x = sites % L_x                            # x position of the sites
 y = sites // L_x                           # y position of the sites
 x, y = GaussianPointSet2D(x, y, width)     # Positions of the sites in the amorphous lattice
-
+S = L_x * L_y                              # Surface of the cross-section
+B = flux / S                               # Magnetic field through the cross-section
 
 # Declarations
-energy_PBC = np.zeros((n_states, len(kz)))
 energy_OBC = np.zeros((n_states, len(kz)))
 # %% Main
 
 # Hamiltonians and band structures
-H_offdiag_PBC = H_offdiag(n_sites, n_orb, n_neighbours, L_x, L_y, x, y, t1, t2, lamb, "Closed")
-H_offdiag_OBC = H_offdiag(n_sites, n_orb, n_neighbours, L_x, L_y, x, y, t1, t2, lamb, "Open")
+H_offdiag_OBC = H_offdiag(n_sites, n_orb, L_x, L_y, x, y, t1, t2, lamb, B, "Open",  n_neighbours)
 for j, k in enumerate(kz):
     print(str(j) + "/" + str(len(kz)))
     H_diag = np.kron(np.eye(n_sites), H_onsite(M, t1, t2, lamb, k))  # Onsite Hamiltonian
-    H_PBC = H_diag + H_offdiag_PBC                                   # Off-diagonal Hamiltonian PBC
     H_OBC = H_diag + H_offdiag_OBC                                   # Off-diagonal Hamiltonian OBC
-    energy_PBC[:, j] = spectrum(H_PBC)[0]                            # PBC bands
     energy_OBC[:, j] = spectrum(H_OBC)[0]                            # OBC bands
 
-for j in range(n_states):
-    # plt.plot(kz, energy_PBC[j, :], '.b', markersize=2)
-    plt.plot(kz, energy_OBC[j, :], 'r', markersize=2)
 
-plt.xlim(-pi, pi)
-plt.xlabel("k")
-plt.ylabel("Energy")
-plt.legend(["OBC"])
-plt.title(" M=" + str(M))
+
+
+# %% Figures
+
+font = {'family': 'serif', 'color': 'black', 'weight': 'normal', 'size': 13, }
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rc('font', size=20)
+
+fig, ax = plt.subplots(figsize=(8, 6))
+for j in range(n_states):
+    ax.plot(kz, energy_OBC[j, :], '.b', markersize=0.05)
+
+# Axis labels and limits
+ax.set_ylabel("$E$", fontsize=25)
+ax.set_xlabel("$ka$", fontsize=25)
+ax.set_xlim(-pi, pi)
+# ax.set_ylim(-3, 2)
+
+plt.title(" $w=$ " + str(width) + "," + " $M=$ " + str(M) + ", $\phi / \phi_0=$ " + str(flux))
 plt.show()
+
+
+
