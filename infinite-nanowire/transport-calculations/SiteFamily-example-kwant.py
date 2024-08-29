c384aa3f4cb103a8e0102120d67d17184ee9bde7
Miguel <miguelmartinezmiquel@gmail.com>
2024-08-29 14:36:38 +0200

    Solved hopping problem

--- infinite-nanowire/transport-calculations/SiteFamily-example-kwant.py
+++ infinite-nanowire/transport-calculations/SiteFamily-example-kwant.py
@@ -2,6 +2,7 @@
 
 # Math
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Kwant
 import kwant
@@ -32,29 +33,39 @@ class Scattering_CrossSection(kwant.builder.SiteFamily):
         return np.concatenate((self.coords[tag[0], :], np.array([tag[1]])))
 
     def normalize_tag(self, tag):
-        if tag[0] >= len(self.coords):
-            raise ValueError
         return ta.array(tag)
 
     def __hash__(self):
         return 1
 
-cross_section = AmorphousLattice_2d(Nx=5, Ny=5, w=0.1, r=1.4)
+cross_section = AmorphousLattice_2d(Nx=3, Ny=3, w=0.1, r=1.4)
 cross_section.build_lattice()
 latt = Scattering_CrossSection(norbs=1, cross_section=cross_section, name='wire')
 
 n_layers = 5
 syst = kwant.Builder()
+pos_try = latt(4, 0)
 
-for i in range(latt.Nsites):
-    syst[(latt(i, z) for z in range(n_layers))] = 1.
-    syst[((latt(i, z), latt(i, z + 1)) for z in range(n_layers - 1))] = 2.
-    syst[latt(0, 0), latt(1, 0)] = 4.
-    for neigh in cross_section.neighbours[i]:
-        syst[((latt(i, z), latt(neigh, z)) for z in range(n_layers))] = 4.
+# Define sites and onsite energy at the same time
+syst[(latt(i, z) for i in range(latt.Nsites) for z in range(n_layers))] = 1.
 
-
-
+for z in range(n_layers - 1):
+    syst[((latt(i, z), latt(i, z + 1)) for i in range(latt.Nsites))] = 2.
+    syst[((latt(i, z), latt(n, z)) for i in range(latt.Nsites) for n in cross_section.neighbours[i])] = 5.
 
 
-kwant.plot(syst)
+
+
+    # syst[((latt(i, z), latt(i, z + 1)) for z in range(n_layers - 1))] = 2.
+    #
+    #
+    # # syst[latt(1, 0), latt(1, 1)] = 4.
+    # for neigh in cross_section.neighbours[i]:
+    #      syst[((latt(i, z), latt(neigh, z)) for z in range(n_layers))] = 4.
+
+
+
+
+fig = plt.figure(figsize=(8, 5))
+ax = fig.gca()
+kwant.plot(syst, hop_color='royalblue', hop_lw=0.05, site_size=0.1, site_color='m', ax=ax)
