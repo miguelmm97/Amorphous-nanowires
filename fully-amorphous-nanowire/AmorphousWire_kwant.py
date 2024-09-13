f2059fd077f9b3cab2af181a7eeb21da6984ef7d
Miguel <miguelmartinezmiquel@gmail.com>
2024-09-13 14:48:34 +0200

    Promoted to kwant (unsuccessful)

--- fully-amorphous-nanowire/AmorphousWire_kwant.py
+++ fully-amorphous-nanowire/AmorphousWire_kwant.py
@@ -63,31 +63,6 @@ def hopping(t, lamb, lamb_z, d, phi, theta, cutoff_dist):
 def onsite(eps):
     return eps * np.kron(sigma_x, tau_0)
 
-def displacement2D_kwant(site1, site0):
-    x1, y1 = site0.pos[0], site0.pos[1]
-    x2, y2 = site1.pos[0], site1.pos[1]
-
-    v = np.zeros((2,))
-    v[0] = (x2 - x1)
-    v[1] = (y2 - y1)
-
-    # Norm of the vector between sites 2 and 1
-    r = np.sqrt(v[0] ** 2 + v[1] ** 2)
-
-    # Phi angle of the vector between sites 2 and 1 (angle in the XY plane)
-    if v[0] == 0:  # Pathological case, separated to not divide by 0
-        if v[1] > 0:
-            phi = pi / 2  # Hopping in y
-        else:
-            phi = 3 * pi / 2  # Hopping in -y
-    else:
-        if v[1] > 0:
-            phi = np.arctan2(v[1], v[0])  # 1st and 2nd quadrants
-        else:
-            phi = 2 * pi + np.arctan2(v[1], v[0])  # 3rd and 4th quadrants
-
-    return r, phi
-
 def displacement3D_kwant(site1, site0):
     x1, y1, z1 = site0.pos[0], site0.pos[1], site0.pos[2]
     x2, y2, z2 = site1.pos[0], site1.pos[1], site1.pos[2]
@@ -142,8 +117,6 @@ def Peierls_kwant(site1, site0, flux, area):
         I = quad(integrand, x1, x2, args=(m, x1, y1))[0]
     return np.exp(2 * pi * 1j * flux * I / area)
 
-
-
 """
 Note that in the following hoppings are always defined down up, that is from x to x+1, y to y+1 z to z+1, so that we
 get the angles correctly. Kwant then takes the complex conjugate for the reverse ones.
@@ -152,8 +125,8 @@ Note also that in kwant the hoppings are defined like (latt(), latt()) where the
 which we hopp.
 """
 
-class AmorphousCrossSectionWire_ScatteringRegion(kwant.builder.SiteFamily):
-    def __init__(self, norbs, cross_section, name=None):
+class FullyAmorphousWire_ScatteringRegion(kwant.builder.SiteFamily):
+    def __init__(self, norbs, lattice, name=None):
 
         if norbs is not None:
             if int(norbs) != norbs or norbs <= 0:
@@ -163,15 +136,16 @@ class AmorphousCrossSectionWire_ScatteringRegion(kwant.builder.SiteFamily):
         # Class fields
         loger_kwant.trace('Initialising cross section as a SiteFamily...')
         self.norbs = norbs
-        self.coords = np.array([cross_section.x, cross_section.y]).T
-        self.Nsites = cross_section.Nsites
-        self.Nx = cross_section.Nx
-        self.Ny = cross_section.Ny
+        self.coords = np.array([lattice.x, lattice.y, lattice.z]).T
+        self.Nsites = lattice.Nsites
+        self.Nx = lattice.Nx
+        self.Ny = lattice.Ny
+        self.Nz = lattice.Nz
         self.name = name
         self.canonical_repr = "1" if name is None else name
 
     def pos(self, tag):
-        return np.concatenate((self.coords[tag[0], :], np.array([tag[1]])))
+        return self.coords[tag, :]
 
     def normalize_tag(self, tag):
         return ta.array(tag)
@@ -179,7 +153,7 @@ class AmorphousCrossSectionWire_ScatteringRegion(kwant.builder.SiteFamily):
     def __hash__(self):
         return 1
 
-def promote_to_kwant_nanowire(cross_section, n_layers, param_dict, attach_leads=True, mu_leads=0.):
+def promote_to_kwant_nanowire(lattice_tree, param_dict, attach_leads=True, mu_leads=0.):
 
     # Load parameters into the builder namespace
     try:
@@ -191,38 +165,31 @@ def promote_to_kwant_nanowire(cross_section, n_layers, param_dict, attach_leads=
         raise KeyError(f'Parameter error: {err}')
 
     # Create lattice structure for the scattering region from the amorphous cross-section
-    latt = AmorphousCrossSectionWire_ScatteringRegion(norbs=4, cross_section=cross_section, name='scatt_region')
+    latt = FullyAmorphousWire_ScatteringRegion(norbs=4, lattice=lattice_tree, name='scatt_region')
 
     # Initialise kwant system
     loger_kwant.info('Creating kwant scattering region...')
     syst = kwant.Builder()
-    syst[(latt(i, z) for i in range(latt.Nsites) for z in range(n_layers))] = onsite(eps)
+    syst[(latt(i) for i in range(latt.Nsites))] = onsite(eps)
 
     # Hopping functions
-    hopp_z_up = hopping(t, lamb, lamb_z, 1., 0, 0, cross_section.r)
-    def hopp_cross_section(site1, site0, flux):
-        d, phi = displacement2D_kwant(site1, site0)
-        return hopping(t, lamb, lamb_z, d, phi, pi / 2, cross_section.r) * Peierls_kwant(site1, site0, flux, cross_section.area)
+    def hopp(site1, site0, flux):
+        d, phi, theta = displacement3D_kwant(site1, site0)
+        return hopping(t, lamb, lamb_z, d, phi, theta, lattice_tree.r) * Peierls_kwant(site1, site0, flux, lattice_tree.area)
 
     # Populate hoppings
     for i in range(latt.Nsites):
-        for n in cross_section.neighbours[i]:
+        for n in lattice_tree.neighbours[i]:
             loger_kwant.trace(f'Defining hopping from site {i} to {n}.')
-
-            # In the cross-section
-            syst[((latt(n, z), latt(i, z)) for z in range(n_layers))] = hopp_cross_section
-
-            # Between cross-sections
-            loger_kwant.trace(f'Defining hopping of site {i} between cross-section layers.')
-            syst[((latt(i, z + 1), latt(i, z)) for z in range(n_layers - 1))] = hopp_z_up
+            syst[(latt(n), latt(i))] = hopp
 
     if attach_leads:
-        complete_system = attach_cubic_leads(syst, cross_section, latt, n_layers, param_dict, mu_leads=mu_leads)
+        complete_system = attach_cubic_leads(syst, lattice_tree, latt, param_dict, mu_leads=mu_leads)
     else:
         complete_system = syst
     return complete_system
 
-def attach_cubic_leads(scatt_region, cross_section, latt, n_layers, param_dict, mu_leads=0.):
+def attach_cubic_leads(scatt_region, lattice_tree, latt, param_dict, mu_leads=0.):
 
     # Load parameters into the builder namespace
     try:
@@ -236,13 +203,13 @@ def attach_cubic_leads(scatt_region, cross_section, latt, n_layers, param_dict, 
     onsite_leads = onsite(eps) + mu_leads * np.kron(sigma_0, tau_0)
 
     # Hoppings
-    def hopp_x_up(site1, site0, flux):
-        return hopping(t, lamb, lamb_z, 1., 0, pi / 2, cross_section.r) * Peierls_kwant(site1, site0, flux, cross_section.area)
     def hopp_lead_wire(site1, site0, flux):
         d, phi, theta = displacement3D_kwant(site1, site0)
-        return hopping(t, lamb, lamb_z, d, phi, theta, cross_section.r) * Peierls_kwant(site1, site0, flux, cross_section.area)
-    hopp_z_up = hopping(t, lamb, lamb_z, 1., 0, 0, cross_section.r)
-    hopp_y_up = hopping(t, lamb, lamb_z, 1., pi / 2, pi / 2, cross_section.r)
+        return hopping(t, lamb, lamb_z, d, phi, theta, lattice_tree.r) * Peierls_kwant(site1, site0, flux, lattice_tree.area)
+    def hopp_x_up(site1, site0, flux):
+        return hopping(t, lamb, lamb_z, 1., 0, pi / 2, lattice_tree.r) * Peierls_kwant(site1, site0, flux, lattice_tree.area)
+    hopp_z_up = hopping(t, lamb, lamb_z, 1., 0, 0, lattice_tree.r)
+    hopp_y_up = hopping(t, lamb, lamb_z, 1., pi / 2, pi / 2, lattice_tree.r)
 
     # Left lead: definition
     loger_kwant.info('Attaching left lead...')
@@ -262,8 +229,16 @@ def attach_cubic_leads(scatt_region, cross_section, latt, n_layers, param_dict, 
     scatt_region[(latt_lead(i, j, -1) for i in range(latt.Nx) for j in range(latt.Ny))] = onsite_leads
     scatt_region[kwant.builder.HoppingKind((1, 0, 0), latt_lead, latt_lead)] = hopp_x_up
     scatt_region[kwant.builder.HoppingKind((0, 1, 0), latt_lead, latt_lead)] = hopp_y_up
-    scatt_region[(((latt(i + latt.Ny * j, 0), latt_lead(i, j, -1)) for i in range(latt.Nx) for j in range(latt.Ny)))] = hopp_lead_wire
+
+    interface_left = []
+    for i in range(lattice_tree.Nsites):
+        if lattice_tree.z[i] < 0.5:
+            interface_left.append(i)
+
+    for site in interface_left:
+        scatt_region[((latt(site), latt_lead(i, j, -1)) for i in range(latt.Nx) for j in range(latt.Ny))] = hopp_lead_wire
     scatt_region.attach_lead(left_lead)
+
 
     # Right lead: definition
     loger_kwant.info('Attaching right lead...')
@@ -280,135 +255,17 @@ def attach_cubic_leads(scatt_region, cross_section, latt, n_layers, param_dict, 
 
     # Right lead: Attachment
     loger_kwant.trace('Defining the way to attach the lead to the system...')
-    scatt_region[(latt_lead(i, j, n_layers) for i in range(latt.Nx) for j in range(latt.Ny))] = onsite_leads
+    scatt_region[(latt_lead(i, j, latt.Nz - 1) for i in range(latt.Nx) for j in range(latt.Ny))] = onsite_leads
     scatt_region[kwant.builder.HoppingKind((1, 0, 0), latt_lead, latt_lead)] = hopp_x_up
     scatt_region[kwant.builder.HoppingKind((0, 1, 0), latt_lead, latt_lead)] = hopp_y_up
-    scatt_region[(((latt_lead(i, j, n_layers), latt(i + latt.Ny * j, n_layers - 1)) for i in range(latt.Nx)
-                                                                for j in range(latt.Ny)))] = hopp_lead_wire
+
+    interface_right = []
+    for i in range(lattice_tree.Nsites):
+        if lattice_tree.z[i] > (lattice_tree.Nz - 1) - 0.5:
+            interface_right.append(i)
+
+    for site in interface_right:
+        scatt_region[((latt(site), latt_lead(i, j, latt.Nz - 1)) for i in range(latt.Nx) for j in range(latt.Ny))] = hopp_lead_wire
     scatt_region.attach_lead(right_lead)
+
     return scatt_region
-
-def crystal_nanowire_kwant(Nx, Ny, n_layers, param_dict):
-
-    # Load parameters into the builder namespace
-    try:
-        t      = param_dict['t']
-        eps    = param_dict['eps']
-        lamb   = param_dict['lamb']
-        lamb_z = param_dict['lamb_z']
-    except KeyError as err:
-        raise KeyError(f'Parameter error: {err}')
-
-    # Define lattice and initialise system and sites
-    latt = kwant.lattice.cubic(1, norbs=4)
-    syst = kwant.Builder()
-    syst[(latt(i, j, k) for i in range(Nx) for j in range(Ny) for k in range(n_layers))] = onsite(eps)
-
-    # Hoppings
-    cutoff = 1.3
-    def hopp_x_up(site1, site0, flux):
-        return hopping(t, lamb, lamb_z, 1., 0., pi / 2, cutoff) * Peierls_kwant(site1, site0, flux, (Nx - 1) * (Ny - 1))
-    hopp_z_up = hopping(t, lamb, lamb_z, 1., 0., 0, cutoff)
-    hopp_y_up = hopping(t, lamb, lamb_z, 1., pi / 2, pi / 2, cutoff)
-    syst[kwant.builder.HoppingKind((1, 0, 0), latt, latt)] = hopp_x_up
-    syst[kwant.builder.HoppingKind((0, 1, 0), latt, latt)] = hopp_y_up
-    syst[kwant.builder.HoppingKind((0, 0, 1), latt, latt)] = hopp_z_up
-
-    # Lead
-    lead = kwant.Builder(kwant.TranslationalSymmetry((0, 0, -1)))
-    lead[(latt(i, j, 0) for i in range(Nx) for j in range(Ny))] = onsite(eps)
-    lead[kwant.builder.HoppingKind((1, 0, 0), latt, latt)] = hopp_x_up
-    lead[kwant.builder.HoppingKind((0, 1, 0), latt, latt)] = hopp_y_up
-    lead[kwant.builder.HoppingKind((0, 0, 1), latt, latt)] = hopp_z_up
-    syst.attach_lead(lead)
-    syst.attach_lead(lead.reversed())
-    return syst
-
-def infinite_nanowire_kwant(Nx, Ny, param_dict, mu_leads=0.):
-    # Load parameters into the builder namespace
-    try:
-        t = param_dict['t']
-        eps = param_dict['eps']
-        lamb = param_dict['lamb']
-        lamb_z = param_dict['lamb_z']
-    except KeyError as err:
-        raise KeyError(f'Parameter error: {err}')
-
-    onsite_leads = onsite(eps) + mu_leads * np.kron(sigma_0, tau_0)
-
-    # Define lattice and initialise system and sites
-    latt = kwant.lattice.cubic(1, norbs=4)
-    lead = kwant.Builder(kwant.TranslationalSymmetry((0, 0, 1)))
-    lead[(latt(i, j, 0) for i in range(Nx) for j in range(Ny))] = onsite_leads
-
-    # Hoppings
-    cutoff = 1.3
-    def hopp_x_up(site1, site0, flux):
-        return hopping(t, lamb, lamb_z, 1., 0, pi / 2, cutoff) * Peierls_kwant(site1, site0, flux, (Nx - 1) * (Ny - 1))
-    hopp_z_up = hopping(t, lamb, lamb_z, 1., 0, 0, cutoff)
-    hopp_y_up = hopping(t, lamb, lamb_z, 1., pi / 2, pi / 2, cutoff)
-    lead[kwant.builder.HoppingKind((1, 0, 0), latt, latt)] = hopp_x_up
-    lead[kwant.builder.HoppingKind((0, 1, 0), latt, latt)] = hopp_y_up
-    lead[kwant.builder.HoppingKind((0, 0, 1), latt, latt)] = hopp_z_up
-
-    return lead
-
-def FuBerg_model_bands(Nx, Ny, kz, flux, param_dict):
-
-    # Load parameters into the builder namespace
-    try:
-        t = param_dict['t']
-        eps = param_dict['eps']
-        lamb = param_dict['lamb']
-        lamb_z = param_dict['lamb_z']
-    except KeyError as err:
-        raise KeyError(f'Parameter error: {err}')
-
-    # Lattice parameters
-    Nsites = int(Nx * Ny)
-    area = (Nx - 1) * (Ny - 1)
-    dimH = Nsites * 4
-    sites = np.arange(0, Nsites)
-    x = sites % Nx
-    y = sites // Nx
-
-    # Hamiltonian parameters
-    H_offdiag = np.zeros((dimH, dimH), dtype=np.complex128)
-    H = np.zeros((len(kz), dimH, dimH), dtype=np.complex128)
-    def peierls_x(y):
-        return np.exp(2 * pi * 1j * flux * y / area)
-
-    # Off-diagonal Hamiltonian
-    for n in sites:
-        # Jump operators along x and y direction
-        state_n, state_nx, state_ny = np.zeros((Nsites,)), np.zeros((Nsites,)), np.zeros((Nsites,))
-        state_n[n] = 1
-        if x[n] != Nx - 1: state_nx[n + 1] = 1
-        if y[n] != Ny - 1: state_ny[n + Nx] = 1
-        jump_x, jump_y = np.outer(state_n, state_nx) * peierls_x(y[n]), np.outer(state_n, state_ny)
-
-        # Off-diagonal Hamiltonian
-        H_offdiag += -t * np.kron(jump_x, np.kron(sigma_x, tau_0)) - t * np.kron(jump_y, np.kron(sigma_x, tau_0)) + \
-                     1j * 0.5 * lamb * (np.kron(jump_x, np.kron(sigma_z, tau_y)) - np.kron(jump_y, np.kron(sigma_z, tau_x)))
-    H_offdiag += H_offdiag.T.conj()
-
-    # Full Hamiltonian
-    for i, k in enumerate(kz):
-        H[i, :, :] = (eps - 2 * t * np.cos(k)) * np.kron(np.eye(Nsites), np.kron(sigma_x, tau_0)) + \
-                     + lamb_z * np.sin(k) * np.kron(np.eye(Nsites), np.kron(sigma_y, tau_0)) + H_offdiag
-
-    # Band structure
-    energy_bands, eigenstates = {}, {}
-    aux_bands = np.zeros((len(kz), dimH))
-    aux_eigenstates = np.zeros((len(kz), dimH, dimH), dtype=np.complex128)
-    for j in range(len(kz)):
-        bands_k, eigenstates_k = np.linalg.eigh(H[j, :, :])
-        idx = bands_k.argsort()
-        aux_bands[j, :], aux_eigenstates[j, :, :] = bands_k[idx], eigenstates_k[:, idx]
-
-    # Ordering bands
-    for i in range(dimH):
-        energy_bands[i] = aux_bands[:, i]
-        eigenstates[i] = aux_eigenstates[:, :, i]
-
-    return energy_bands, eigenstates
--- fully-amorphous-nanowire/fully-amorphous-checks.py
+++ fully-amorphous-nanowire/fully-amorphous-checks.py
@@ -12,7 +12,7 @@ import kwant
 # Modules
 from functions import *
 from AmorphousLattice_3d import AmorphousLattice_3d
-
+from AmorphousWire_kwant import promote_to_kwant_nanowire
 #%% Logging setup
 loger_main = logging.getLogger('main')
 loger_main.setLevel(logging.INFO)
@@ -53,14 +53,27 @@ t          = 1                          # Hopping
 eps        = 4 * t                      # Onsite orbital hopping (in units of t)
 lamb       = 1 * t                      # Spin-orbit coupling in the cross-section (in units of t)
 lamb_z     = 1.8 * t                    # Spin-orbit coupling along z direction
+params_dict = {'t': t, 'eps': eps, 'lamb': lamb, 'lamb_z': lamb_z}
+mu_leads = 0 * t
 
 
 #%% Main
 
 # Initiaise amorphous nanowire for transport
 loger_main.info('Generating amorphous lattice...')
-nanowire = AmorphousLattice_3d(Nx=Nx, Ny=Ny, Nz=Nz, w=width, r=1.3)
-nanowire.build_lattice()
+lattice = AmorphousLattice_3d(Nx=Nx, Ny=Ny, Nz=Nz, w=width, r=1.3)
+lattice.build_lattice()
+nanowire = promote_to_kwant_nanowire(lattice, params_dict, mu_leads=mu_leads).finalized()
+loger_main.info('Nanowire promoted to Kwant successfully.')
+
+
+
+site_size  = 0.1
+site_lw    = 0.01
+site_color = 'm'
+hop_color  = 'royalblue'
+hop_lw     = 0.05
+lead_color = 'r'
 
 
 fig0 = plt.figure(figsize=(10, 4))
@@ -68,6 +81,17 @@ gs = GridSpec(1, 2, figure=fig0)
 ax0_0 = fig0.add_subplot(gs[0, 0], projection='3d')
 ax0_1 = fig0.add_subplot(gs[0, 1])
 
-nanowire.plot_lattice(ax0_0)
-nanowire.plot_lattice_projection(ax0_1)
+lattice.plot_lattice(ax0_0)
+lattice.plot_lattice_projection(ax0_1)
+
+
+fig1 = plt.figure(figsize=(6, 6))
+gs = GridSpec(1, 1, figure=fig1)
+ax1 = fig1.add_subplot(gs[0, 0], projection='3d')
+kwant.plot(nanowire, site_size=site_size, site_lw=site_lw, site_color=site_color, hop_lw=hop_lw, hop_color=hop_color,
+           lead_site_size=site_size, lead_color=lead_color, lead_site_lw=site_lw, lead_hop_lw=hop_lw,
+           ax=ax1)
+ax1.set_axis_off()
+ax1.margins(-0.49, -0.49, -0.49)
+
 plt.show()
