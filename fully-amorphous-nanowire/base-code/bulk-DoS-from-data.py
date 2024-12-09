b020f5a3fbdd750ed3905ff4cdd42b69fbd0f3d7
Miguel <miguelmartinezmiquel@gmail.com>
2024-12-09 12:00:04 +0100

    Local bulk Dos now plotting fast

---
+++ fully-amorphous-nanowire/base-code/bulk-DoS-from-data.py
@@ -0,0 +1,199 @@
+#%% modules set up
+
+# Math and plotting
+import numpy as np
+
+# Kwant
+import kwant
+
+# modules
+from modules.functions import *
+from modules.AmorphousLattice_3d import AmorphousLattice_3d
+from modules.FullyAmorphousWire_kwant import promote_to_kwant_nanowire3d, select_perfect_transmission_flux
+
+import sys
+from datetime import date
+
+#%% Logging setup
+loger_main = logging.getLogger('main')
+loger_main.setLevel(logging.INFO)
+
+stream_handler = colorlog.StreamHandler()
+formatter = ColoredFormatter(
+    '%(black)s%(asctime) -5s| %(blue)s%(name) -10s %(black)s| %(cyan)s %(funcName) '
+    '-40s %(black)s|''%(log_color)s%(levelname) -10s | %(message)s',
+    datefmt=None,
+    reset=True,
+    log_colors={
+        'TRACE': 'black',
+        'DEBUG': 'purple',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    },
+    secondary_log_colors={},
+    style='%'
+)
+
+stream_handler.setFormatter(formatter)
+loger_main.addHandler(stream_handler)
+
+
+#%% Loading data
+file_list = ['Exp23.h5']
+data_dict = load_my_data(file_list, '/home/mfmm/Projects/amorphous-nanowires/data/data-cond-vs-flux-fully-amorphous')
+
+# Parameters
+Ef           = data_dict[file_list[0]]['Parameters']['Ef']
+Nx           = data_dict[file_list[0]]['Parameters']['Nx']
+Ny           = data_dict[file_list[0]]['Parameters']['Ny']
+Nz           = data_dict[file_list[0]]['Parameters']['Nz']
+r            = data_dict[file_list[0]]['Parameters']['r']
+t            = data_dict[file_list[0]]['Parameters']['t']
+eps          = data_dict[file_list[0]]['Parameters']['eps']
+lamb         = data_dict[file_list[0]]['Parameters']['lamb']
+lamb_z       = data_dict[file_list[0]]['Parameters']['lamb_z']
+mu_leads     = data_dict[file_list[0]]['Parameters']['mu_leads']
+params_dict = {'t': t, 'eps': eps, 'lamb': lamb, 'lamb_z': lamb_z}
+
+# Simulation data
+flux          = data_dict[file_list[0]]['Simulation']['flux']
+G_array       = data_dict[file_list[0]]['Simulation']['G_array']
+width         = data_dict[file_list[0]]['Simulation']['width']
+x             = data_dict[file_list[0]]['Simulation']['x']
+y             = data_dict[file_list[0]]['Simulation']['y']
+z             = data_dict[file_list[0]]['Simulation']['z']
+
+# Variables
+idx1, idx2 = 25, 33 # 100, 144
+flux1 = flux[idx1]
+flux2 = flux[idx2]
+#%% Main
+
+loger_main.info('Generating fully amorphous lattice...')
+lattice = AmorphousLattice_3d(Nx=Nx, Ny=Ny, Nz=Nz, w=width, r=r)
+lattice.set_configuration(x, y, z)
+lattice.build_lattice(restrict_connectivity=False)
+lattice.generate_disorder(K_hopp=0., K_onsite=0.)
+nanowire = promote_to_kwant_nanowire3d(lattice, params_dict, mu_leads=mu_leads).finalized()
+site_pos = np.array([site.pos for site in nanowire.id_by_site])
+loger_main.info('Nanowire promoted to Kwant successfully.')
+
+# Calculating the scattering wavefunctions at certain energies
+loger_main.info('Calculating scattering wave functions...')
+state1 = kwant.wave_function(nanowire, energy=Ef[0], params=dict(flux=flux1))
+state2 = kwant.wave_function(nanowire, energy=Ef[0], params=dict(flux=flux2))
+loger_main.info('Scattering wave functions calculated successfully')
+
+# Total local density through a cut
+N = np.linspace(2, Nx / 2, 10)
+bulk_tot_density1 = np.zeros(N.shape)
+bulk_tot_density2 = np.zeros(N.shape)
+
+loger_main.info('Calculating total local bulk DoS...')
+for i, n in enumerate(N):
+    def bulk(site):
+        x, y = site.pos[0] - 0.5 * Nx, site.pos[1] - 0.5 * Ny
+        return (np.abs(x) and np.abs(y)) < n
+    loger_main.info(f'Section {i} / {len(N)}')
+    total_density_operator = kwant.operator.Density(nanowire, where=bulk, sum=True)
+    density_operator = kwant.operator.Density(nanowire, where=bulk, sum=False)
+    bulk_tot_density1[i] = total_density_operator(state1(0)[0])
+    bulk_tot_density2[i] = total_density_operator(state2(0)[0])
+bulk_tot_density1 = bulk_tot_density1 / bulk_tot_density1[-1]
+bulk_tot_density2 = bulk_tot_density2 / bulk_tot_density2[-1]
+
+
+# Local density through a cut
+N_local = np.linspace(2, Nx / 2, 3)
+bulk_density1 = {}
+bulk_density2 = {}
+cut_pos = {}
+
+def bulk(syst, rad):
+    new_sites_x = tuple([site for site in syst.id_by_site if np.abs(site.pos[0] - 0.5 * Nx) < rad])
+    new_sites = tuple([site for site in new_sites_x if np.abs(site.pos[1] - 0.5 * Ny) < rad])
+    new_sites_pos = np.array([site.pos for site in new_sites])
+    return new_sites, new_sites_pos
+
+loger_main.info('Calculating local bulk DoS...')
+for i, n in enumerate(N_local):
+    cut_sites, cut_pos[i] = bulk(nanowire, n)
+    loger_main.info(f'Section {i} / {len(N_local)}')
+    density_operator = kwant.operator.Density(nanowire, where=cut_sites, sum=False)
+    bulk_density1[i] = density_operator(state1(0)[0])
+    bulk_density2[i] = density_operator(state2(0)[0])
+
+for key in bulk_density1.keys():
+    bulk_density1[key] = bulk_density1[key] / np.sum(bulk_density1[len(N_local) - 1])
+    bulk_density2[key] = bulk_density2[key] / np.sum(bulk_density2[len(N_local) - 1])
+
+# Normalisation for the plots
+sigmas = 1
+mean_value = np.mean(np.array([bulk_density1[len(N_local) - 1], bulk_density2[len(N_local) - 1]]))
+std_value = np.std(np.array([bulk_density1[len(N_local) - 1], bulk_density2[len(N_local) - 1]]))
+max_value, min_value = mean_value + sigmas * std_value, 0
+
+
+
+#%% Saving data
+data_dir = '/home/mfmm/Projects/amorphous-nanowires/data/data-bulk-dos'
+file_list = os.listdir(data_dir)
+expID = get_fileID(file_list, common_name='Exp')
+filename = '{}{}{}'.format('Exp', expID, '.h5')
+filepath = os.path.join(data_dir, filename)
+
+
+with h5py.File(filepath, 'w') as f:
+
+    # Simulation folder
+    simulation = f.create_group('Simulation')
+    DoS1 = simulation.create_group('DoS1')
+    DoS2 = simulation.create_group('DoS2')
+    cuts = simulation.create_group('cuts')
+
+    store_my_data(simulation,  'G_array',          G_array)
+    store_my_data(simulation,    'flux',           flux)
+    store_my_data(simulation,   'width',           width)
+    store_my_data(simulation,       'N',           N)
+    store_my_data(simulation, 'N_local',           N_local)
+    store_my_data(simulation, 'bulk_tot_density1', bulk_tot_density1)
+    store_my_data(simulation, 'bulk_tot_density2', bulk_tot_density2)
+
+    store_my_dict(simulation['DoS1'],  bulk_density1)
+    store_my_dict(simulation['DoS2'],  bulk_density2)
+    store_my_dict(simulation['cuts'],  cut_pos)
+
+
+    # Parameters folder
+    parameters = f.create_group('Parameters')
+    store_my_data(parameters, 'flux1', flux1)
+    store_my_data(parameters, 'flux2', flux2)
+    store_my_data(parameters, 'idx1',   idx1)
+    store_my_data(parameters, 'idx2',   idx2)
+    store_my_data(parameters, 'Ef',      Ef)
+    store_my_data(parameters, 'Nx',      Nx)
+    store_my_data(parameters, 'Ny',      Ny)
+    store_my_data(parameters, 'Nz',      Nz)
+    store_my_data(parameters, 'r ',      r)
+    store_my_data(parameters, 't ',      t)
+    store_my_data(parameters, 'eps',     eps)
+    store_my_data(parameters, 'lamb',    lamb)
+    store_my_data(parameters, 'lamb_z',  lamb_z)
+    store_my_data(parameters, 'mu_leads', mu_leads)
+
+    # Attributes
+    attr_my_data(parameters, "Date",       str(date.today()))
+    attr_my_data(parameters, "Code_path",  sys.argv[0])
+
+loger_main.info('Data saved correctly')
+
+
+
+
+
+
+
+
+
---
+++ fully-amorphous-nanowire/plotting/bulk-DoS_plot.py
@@ -0,0 +1,148 @@
+#%% Modules and setup
+
+# Plotting
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import LinearSegmentedColormap, Normalize
+from matplotlib import cm
+
+# Modules
+from modules.functions import *
+
+
+#%% Loading data
+file_list = ['Exp4.h5']
+data_dict = load_my_data(file_list, '/home/mfmm/Projects/amorphous-nanowires/data/data-bulk-dos')
+
+# Parameters
+Ef           = data_dict[file_list[0]]['Parameters']['Ef']
+Nx           = data_dict[file_list[0]]['Parameters']['Nx']
+Ny           = data_dict[file_list[0]]['Parameters']['Ny']
+Nz           = data_dict[file_list[0]]['Parameters']['Nz']
+r            = data_dict[file_list[0]]['Parameters']['r ']
+t            = data_dict[file_list[0]]['Parameters']['t ']
+eps          = data_dict[file_list[0]]['Parameters']['eps']
+lamb         = data_dict[file_list[0]]['Parameters']['lamb']
+lamb_z       = data_dict[file_list[0]]['Parameters']['lamb_z']
+mu_leads     = data_dict[file_list[0]]['Parameters']['mu_leads']
+flux1        = data_dict[file_list[0]]['Parameters']['flux1']
+flux2        = data_dict[file_list[0]]['Parameters']['flux2']
+idx1         = data_dict[file_list[0]]['Parameters']['idx1']
+idx2         = data_dict[file_list[0]]['Parameters']['idx2']
+
+
+# Simulation data
+flux                   = data_dict[file_list[0]]['Simulation']['flux']
+width                  = data_dict[file_list[0]]['Simulation']['width']
+G_array                = data_dict[file_list[0]]['Simulation']['G_array']
+N                      = data_dict[file_list[0]]['Simulation']['N']
+N_local                = data_dict[file_list[0]]['Simulation']['N_local']
+bulk_tot_density1      = data_dict[file_list[0]]['Simulation']['bulk_tot_density1']
+bulk_tot_density2      = data_dict[file_list[0]]['Simulation']['bulk_tot_density2']
+bulk_density1          = data_dict[file_list[0]]['Simulation']['DoS1']
+bulk_density2          = data_dict[file_list[0]]['Simulation']['DoS2']
+cut_pos                = data_dict[file_list[0]]['Simulation']['cuts']
+
+# Normalisation for the plots
+sigmas = 3
+mean_value = np.mean(np.array([bulk_density1[f'{len(N_local) - 1}'], bulk_density2[f'{len(N_local) - 1}']]))
+std_value = np.std(np.array([bulk_density1[f'{len(N_local) - 1}'], bulk_density2[f'{len(N_local) - 1}']]))
+max_value, min_value = mean_value + sigmas * std_value, 0
+
+#%% Figure 1
+font = {'family': 'serif', 'color': 'black', 'weight': 'normal', 'size': 22, }
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+color_list = ['dodgerblue', 'limegreen', 'm', 'r', 'orange']
+fontsize = 15
+
+
+# Figure 1
+fig1 = plt.figure()
+gs = GridSpec(2, 1, figure=fig1, wspace=0.1, hspace=0.35)
+ax1 = fig1.add_subplot(gs[0, 0])
+ax2 = fig1.add_subplot(gs[1, 0])
+
+ax2.plot(N, bulk_tot_density1, marker='o', linestyle='solid', color='limegreen', label='(a)')
+ax2.plot(N, bulk_tot_density2, marker='o', linestyle='solid', color='dodgerblue', label='(b)')
+ax2.set_xlabel('$n_x$', fontsize=fontsize)
+ax2.set_ylabel('Norm. DoS', fontsize=fontsize)
+ax2.legend(frameon=False, fontsize=15)
+
+
+ax_vec = [ax1]
+for i in range(G_array.shape[1]):
+    for j in range(len(Ef)):
+        ax = ax_vec[j]
+        label = None if (j % 2 != 0) else f'$w= {width[i]}$'
+        ax.plot(flux, G_array[j, i, :], color=color_list[i], linestyle='solid', label=label)
+        ax.plot(flux, 1 * np.ones(flux.shape), '--', color='Black', alpha=0.2)
+        ax.text(flux1 + 0.05, G_array[j, i, idx1] + 0.2, f'$(a)$', fontsize=fontsize)
+        ax.text(flux2 + 0.05, G_array[j, i, idx2] - 0.2, f'$(b)$', fontsize=fontsize)
+        ax.plot(flux2, G_array[j, i, idx2], 'ob')
+        ax.plot(flux1, G_array[j, i, idx1], 'ob')
+
+# Figure 1: Format
+# ax1.legend(ncol=5, frameon=False, fontsize=20)
+ylim = np.max(G_array) + 0.2
+for ax in ax_vec:
+    ax.set_xlim(flux[0], flux[-1])
+    ax.set_ylim(0, ylim)
+    ax.tick_params(which='major', width=0.75, labelsize=10)
+    ax.tick_params(which='major', length=6, labelsize=10)
+    ax.set_xlabel("$\phi$", fontsize=fontsize)
+    ax.set_ylabel("$G[2e^2/h]$", fontsize=fontsize)
+
+#%% Figure 2
+
+# Defining a colormap
+color_map = plt.get_cmap("magma").reversed()
+colors = color_map(np.linspace(0, 1, 20))
+colors[0] = [1, 1, 1, 1]
+color_map = LinearSegmentedColormap.from_list("custom_colormap", colors)
+colormap = cm.ScalarMappable(norm=Normalize(vmin=min_value, vmax=max_value), cmap=color_map)
+
+
+# Figure 1
+fig2 = plt.figure(figsize=(12, 10))
+gs = GridSpec(2, 4, figure=fig2, wspace=0.1, hspace=0.02)
+ax1 = fig2.add_subplot(gs[0, 0], projection='3d')
+ax2 = fig2.add_subplot(gs[0, 1], projection='3d')
+ax3 = fig2.add_subplot(gs[0, 2], projection='3d')
+ax4 = fig2.add_subplot(gs[1, 0], projection='3d')
+ax5 = fig2.add_subplot(gs[1, 1], projection='3d')
+ax6 = fig2.add_subplot(gs[1, 2], projection='3d')
+
+
+
+for i, ax in enumerate(fig2.axes):
+    if i<3:
+        ax.set_title(f'$n_x, n_y < {N_local[i] :.2f}$')
+        ax.scatter(cut_pos[f'{i}'][:, 0], cut_pos[f'{i}'][:, 1], cut_pos[f'{i}'][:, 2], facecolor='white', edgecolor='black')
+        ax.scatter(cut_pos[f'{i}'][:, 0], cut_pos[f'{i}'][:, 1], cut_pos[f'{i}'][:, 2], c=bulk_density1[f'{i}'], cmap=color_map, vmin=min_value,
+                vmax=max_value)
+    else:
+        ax.set_title(f'$n_x, n_y < {N_local[i - 3] :.2f}$')
+        ax.scatter(cut_pos[f'{i - 3}'][:, 0], cut_pos[f'{i - 3}'][:, 1], cut_pos[f'{i - 3}'][:, 2], facecolor='white', edgecolor='black')
+        ax.scatter(cut_pos[f'{i - 3}'][:, 0], cut_pos[f'{i - 3}'][:, 1], cut_pos[f'{i - 3}'][:, 2], c=bulk_density2[f'{i - 3}'], cmap=color_map,
+                   vmin=min_value, vmax=max_value)
+
+    ax.set_box_aspect((1, 1, 5))
+    ax.set_axis_off()
+
+
+cbar_ax = fig2.add_subplot(gs[0, -1])
+divider = make_axes_locatable(cbar_ax)
+cax = divider.append_axes("left", size="10%", pad=0)
+cbar = fig2.colorbar(colormap, cax=cax, orientation='vertical')
+cbar_ax.set_axis_off()
+cbar.set_label(label='$\\vert \psi (r)\\vert ^2$', labelpad=10, fontsize=20)
+
+
+fig1.savefig(f'../figures/{file_list[0]}-total-DoS-bulk.pdf', format='pdf')
+fig2.savefig(f'../figures/{file_list[0]}-bulk-cuts.pdf', format='pdf')
+
+plt.show()
